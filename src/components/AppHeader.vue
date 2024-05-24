8ad6e24d84a08dd6b429eb8ed8fa9957bb902cc8
Josuel Garcia <garciajosuel@yahoo.com>
2024-05-24 16:32:56 -0400

    Created App Header component & removed unwanted files from scaffolding the project.

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="app-header">
+    <h1>Contact Manager</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+};
+</script>
+
+<style scoped>
+.app-header {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background-color: #333;
+  color: white;
+  text-align: center;
+  padding: 1rem;
+}
+</style>
